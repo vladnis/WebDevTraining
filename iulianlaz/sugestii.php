2a59ba4d6063fff15fec596b87d8d866b312ca0d
Iulian Lazar <iulianlaz@gmail.com>
2012-12-19 20:25:49 -0800

    Tenis rankings
    Extract tennis rankings

---
+++ iulianlaz/sugestii.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Fill up array with names
+$a[]="Anna";
+$a[]="Brittany";
+$a[]="Cinderella";
+$a[]="Diana";
+$a[]="Eva";
+$a[]="Fiona";
+$a[]="Gunda";
+$a[]="Hege";
+$a[]="Inga";
+$a[]="Johanna";
+$a[]="Kitty";
+$a[]="Linda";
+$a[]="Nina";
+$a[]="Ophelia";
+$a[]="Petunia";
+$a[]="Amanda";
+$a[]="Raquel";
+$a[]="Cindy";
+$a[]="Doris";
+$a[]="Eve";
+$a[]="Evita";
+$a[]="Sunniva";
+$a[]="Tove";
+$a[]="Unni";
+$a[]="Violet";
+$a[]="Liza";
+$a[]="Elizabeth";
+$a[]="Ellen";
+$a[]="Wenche";
+$a[]="Vicky";
+
+//get the q parameter from URL
+$varq=$_GET["q"];
+
+//lookup all hints from array if length of q>0
+if (strlen($varq) > 0)
+  {
+  $hint="";
+  for($i=0; $i<count($a); $i++)
+    {
+    if (strtolower($varq)==strtolower(substr($a[$i],0,strlen($varq))))
+      {
+      if ($hint=="")
+        {
+        $hint=$a[$i];
+        }
+      else
+        {
+        $hint=$hint." , ".$a[$i];
+        }
+      }
+    }
+  }
+
+// Set output to "no suggestion" if no hint were found
+// or to the correct values
+if ($hint == "")
+  {
+  $response="no suggestion";
+  }
+else
+  {
+  $response=$hint;
+  }
+
+//output the response
+echo $response;
+
+?>
